ad900f64bf8e7ed4ddbd6dcf60d1bddcf79e4781
Thomas Galinier <galinierthomas@gmail.com>
2025-08-22 22:32:32 +0200

    landing page in progress

--- src/routes/_client/index.tsx
+++ src/routes/_client/index.tsx
@@ -1,3 +1,4 @@
+import { CardService } from "@/components/Client/CardService";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { createFileRoute } from "@tanstack/react-router";
@@ -9,9 +10,9 @@ export const Route = createFileRoute("/_client/")({
 
 function RouteComponent() {
 	return (
-		<main className="px-24">
-			<section className="flex flex-col lg:flex-row lg:items-stretch gap-24">
-				<div className="lg:w-1/2 flex gap-5 flex-col">
+		<main>
+			<section className="flex flex-col lg:flex-row lg:items-stretch gap-24 bg-blue-100 px-24 h-3/4 content-center">
+				<div className="lg:w-1/2 flex gap-5 flex-col m-auto py-10 lg:p-0">
 					<h1 className="font-bold text-5xl">
 						Votre vélo réparé <br /> sans bouger de chez vous
 					</h1>
@@ -27,16 +28,57 @@ function RouteComponent() {
 						</Button>
 					</div>
 					<div className="flex gap-2">
-						<Badge variant='outline' className="flex items-center w-fit gap-2"><ClockIcon size={14}/>Créneaux le jour même</Badge>
-						<Badge variant='outline' className="flex items-center w-fit gap-2"><MapPinIcon size={14}/>Nous venons à vous</Badge>
+						<Badge variant="outline" className="flex items-center w-fit gap-2">
+							<ClockIcon size={14} />
+							Créneaux le jour même
+						</Badge>
+						<Badge variant="outline" className="flex items-center w-fit gap-2">
+							<MapPinIcon size={14} />
+							Nous venons à vous
+						</Badge>
 					</div>
 				</div>
-				<div className="hidden lg:block lg:w-1/2 rounded-lg shadow-md overflow-hidden">
-					<img
-						src="/hero-img2.png"
-						alt="Description de l'image"
-						className="h-[400px] w-full object-cover"
+				<img
+					src="/hero-img2.png"
+					alt="Description de l'image"
+					className="h-3/4 object-cover lg:w-1/2 rounded-lg m-auto hidden lg:block"
+				/>
+			</section>
+			<section className="px-24 mt-10 h-3/4">
+				<div className="flex flex-col gap-2 py-4 ">
+					<h2 className="font-bold text-4xl">
+						Nos services de réparation à domicile
+					</h2>
+					<p className="text-slate-700">
+						Du simple réglage aux réparations complètes, nos mécaniciens se
+						déplacent jusqu’à vous. Découvrez nos prestations rapides,
+						transparentes et adaptées à chaque besoin.
+					</p>
+				</div>
+				<div className="flex flex-wrap gap-4  w-full">
+					<CardService
+						title="Révision générale"
+						description="Un révision complète pour assurer la sécurité et la performance de votre vélo."
 					/>
+					<CardService
+						title="Réparation de pneus"
+						description="Réparation rapide de vos pneus pour repartir en toute sécurité, où que vous soyez."
+					/>
+					<CardService
+						title="Entretien transmission"
+						description="Réglage et optimisation de la chaîne et des vitesses pour un pédalage fluide et sans accroc."
+					/>
+					<CardService
+						title="Révision des freins"
+						description="Contrôle et réglage pour un freinage puissant et sécurisé en toutes conditions."
+					/>
+					<CardService
+						title="Entretien roues & rayons"
+						description="Réglage et dévoilage pour une roue parfaitement équilibrée et durable."
+					/>
+					<CardService
+						title="Montage & réglages"
+						description="Assemblage complet et ajustements précis pour un vélo prêt à rouler dès la première sortie."/>
 				</div>
 			</section>
 		</main>

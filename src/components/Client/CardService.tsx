839affd9a50e01859627b1d901681552577592b6
Thomas Galinier <galinierthomas@gmail.com>
2025-08-25 20:20:39 +0200

    fix responsive

--- src/components/Client/CardService.tsx
+++ src/components/Client/CardService.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent, CardHeader } from "../ui/card";
 
 export function CardService({ title, description }: { title: string; description: string}) {
 	return (
-		<Card className="w-1/4">
+		<Card className="w-full  lg:w-1/4">
 			<CardHeader className="flex font-bold">
 				{title}
 			</CardHeader>
